34fef80bd3ae1fdcf2afb4a3fae7fb80c20fcf1e
JtylerK <jefftyler89@gmail.com>
2016-03-26 16:43:20 -0600

    Added vampier detection questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "what is your name?"
+name = gets.chomp
+
+puts "how old are you?"
+age = gets.chomp
+
+puts "what year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+reply = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
