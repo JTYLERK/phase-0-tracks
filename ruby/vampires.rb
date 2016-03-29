c72a80b698a21864ad65fe044fa458f1085a389b
JtylerK <jefftyler89@gmail.com>
2016-03-28 21:01:30 -0700

    Added dectection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,23 +3,34 @@ number_of_employees = gets.chomp
 
 until number_of_employees == 0 
 
-puts "what is your name?"
-name = gets.chomp
+	puts "what is your name?"
+	name = gets.chomp
 
-puts "how old are you?"
-age = gets.chomp
+	puts "how old are you?"
+	age = gets.chomp
 
-puts "what year were you born?"
-birth_year = gets.chomp
+	puts "what year were you born?"
+	birth_year = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-reply = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	reply = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+	puts "Would you like to enroll in the company's health insurance? (Y or N)"
+	insurance = gets.chomp
+
+	number_of_employees = number_of_employees - 1 
+
+end
 
 
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
+if age == (2016 - birth_year) && reply == "y" ||  insurance == "y"
+	result = "Probably not a vampire"
+elsif age!= (2016 - birth_year) && reply == "n" || insurance == "n"
+	result = "Probably a vampire"
+elsif 	age != (2016 -birth_year) && reply == "n" && insurance =="n"
+	result = "Almost certainly a vampire"
+elsif name == "Drake Cula" || name == "Tu Fang"
+	result = "Definitely a vampire"
+else
+	result = "Not really sure"
+end
