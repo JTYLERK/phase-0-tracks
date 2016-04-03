f66df31ad8a4d02ef7bdccfae2c9ce8442c93048
JtylerK <jefftyler89@gmail.com>
2016-04-03 13:10:08 -0700

    Used pseudocode to outline project

---
+++ ruby/hashes.rb
@@ -0,0 +1,15 @@
+=begin
+	
+Promp users for the following information:
+
+Name:
+Age:
+Number of Children:
+Decor Theme:
+Number of Rooms:
+Favorite Color:
+Do they like to host parties (Yes or No):
+What is their budget?:
+City or Suburbs?(pick one):
+	
+=end
