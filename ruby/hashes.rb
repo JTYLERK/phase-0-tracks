5ee1a94b24084c55a9a86995770aa63905ad2941
JtylerK <jefftyler89@gmail.com>
2016-04-03 13:52:54 -0700

    Prints users input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -17,42 +17,57 @@ City or Suburbs?(pick one):
 user_info = {}
 
 p "What is your name?: "
-name = gets.chomp
+user_info[:name] = gets.chomp
 puts
 
 p "How old are you?: "
-age = gets.chomp.to_i
+user_info[:age] = gets.chomp.to_i
 puts
 
 p "How many children do you have?: "
-children = gets.chomp.to_i
+user_info[:children] = gets.chomp.to_i
 puts
 
 p "What decor theme would you like?: "
-theme = gets.chomp
+user_info[:theme] = gets.chomp
 puts
 
 p "How many rooms do you have?: "
-rooms = gets.chomp
+user_info[:rooms] = gets.chomp
 puts
 
 p "What is your favorite color?: "
-color = gets.chomp
+user_info[:color] = gets.chomp
 puts
 
 p "Do you like to host parties?: "
-host_parties = gets.chomp
+if gets.chomp == "yes"
+	user_info[:parties] = true
+else
+	user_info[:parties] = false
+end
 puts
 
-p "What is your budget? (use numbers to express dolar amount) "
-budget = gets.chomp.to_i
+p "What is your budget? (use only numbers to express dolar amount) "
+user_info[:budget] = gets.chomp.to_i
 puts
 
 p "Where do you live? (Pick one: City or Suburbs): "
-place = gets.chomp
+user_info[:place] = gets.chomp
 puts
 
 
+puts "
+	name: #{user_info[:name]}
+	age: #{user_info[:age]}
+	chilren: #{user_info[:children]}
+	theme: #{user_info[:theme]}
+	rooms: #{user_info[:rooms]}
+	color: #{user_info[:color]}
+	likes to host: #{user_info[:parties]}
+	budget: #{user_info[:budget]}
+	location: #{user_info[:place]}
+"
 	
 
 
