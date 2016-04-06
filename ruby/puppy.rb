67f91b11e38e1cf51ddee657f3f6472257a57f20
JtylerK <jefftyler89@gmail.com>
2016-04-05 19:55:40 -0700

    Added Puh P's

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+#SPECIES------------------
+Puh P's 
+
+#CHARACTERISTICS----------
+has fur: true 
+good with kids: true
+fur color: varies
+size: varies
+legs: 4
+ears: 2
+
+#BEHAVIOR-----------------
+fetches
+eats shoes
+plays hide and seek
+sleeps
+does handstands
+=end
