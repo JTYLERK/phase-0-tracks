5848d533b3152b0d3564b2c9c5b4fd741f5a87d6
JtylerK <jefftyler89@gmail.com>
2016-04-06 21:14:07 -0700

    Added cookie and initalize methods to class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,15 @@
 class Santa
+	
+	def initialize
+		puts "initializing Santa instance"
+	end
+	
 	def speak
 		puts "Ho, ho, ho! Haaaaapy holidays!"
 	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"	
+	end
+
 end 
