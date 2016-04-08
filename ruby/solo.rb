76f357995a213ff0353fa48ba8e44d91e36d71e0
JtylerK <jefftyler89@gmail.com>
2016-04-08 14:21:48 -0700

    Initial Commit

---
+++ ruby/solo.rb
@@ -0,0 +1,10 @@
+#Release 0 ----------------------------------------
+=begin
+design a class representing anything
+have at least 3 attributes - use at least 2 data types
+have at least 3 methods
+=end
+
+#define a class 
+class X_Men
+end
