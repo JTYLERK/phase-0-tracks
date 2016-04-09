6cb6af000b2bf171999bf42b188802053e5d55e9
JtylerK <jefftyler89@gmail.com>
2016-04-08 18:42:31 -0700

    added interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -6,16 +6,31 @@ have at least 3 methods
 =end
 
 #define a class 
-class X_Men
+class X_Men_Characters
 	def initialize
-		puts "initializing X-Men instance"
+		puts "initializing new X-Men character."
 		@mutant_level = [1,2,3,4,5]
 		@mutant = {"Jean Gray" => "Telepathy", "Mystique" => "shapeshifting", "Cyclops" => "laser eyes", "Wolverene" => "health regeneration", "Storm" => "weather control", "Magnito" => "metal control" }
 		@gender = ["Male", "Female"]
 
 	end
+
+	def attacks
+		mutant_array = @mutant.to_a
+		mutant_array[rand(0..5)]
+	end
+
+	def method_name
+		
+	end
+
+	def method_name
+		
+	end
 end
 
+player1 = X_Men_Characters.new
+p player1.attacks
 
 #Release 1  ----------------------------------------
 =begin
@@ -23,3 +38,23 @@ use attr_reader and attr_accessor
 use def initalize
 	end 
 =end	
+
+
+#add interface
+p "This game can have up to 6 players. How many players are there?"
+num_of_players = gets.chomp.to_i
+
+num_of_players.times do 
+	new_player = X_Men_Characters.new	
+end
+
+
+
+
+
+
+
+
+
+
+
