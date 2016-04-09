2dc28ee035d2fb620296cfc0b94675f724c682a2
JtylerK <jefftyler89@gmail.com>
2016-04-09 10:54:42 -0700

    Final commit

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,13 +1,28 @@
-class Calculator
-  def add(x,y)
-    x + y
+
+class TodoList
+
+  def initialize(chore_item_1, chore_item_2)
+    @chore_item_1 = chore_item_1
+    @chore_item_2 = chore_item_2
+    @chores = []
+    @chores << chore_item_1
+    @chores << chore_item_2
   end
 
-  def subtract(x,y)
-    x - y
+  def get_items
+    @chores
   end
 
-  def multiply(x,y)
-    x * y
+  def add_item(list_item)
+    @chores << list_item
+  end
+
+  def delete_item(list_item)
+    @chores.delete(list_item)
+  end
+
+  def get_item(index)
+    @chores[index]
   end
 end
+
