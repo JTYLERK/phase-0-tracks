eb3e004003f86944831dfe51c4b1c60ce035ec6c
JtylerK <jefftyler89@gmail.com>
2016-04-10 22:16:58 -0700

    Added Release 1

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,53 +1,30 @@
-#Release 0 ----------------------------------------
-=begin
-design a class representing anything
-have at least 3 attributes - use at least 2 data types
-have at least 3 methods
-=end
 
-#define a class 
-class X_Men_Characters
-	def initialize
-		puts "initializing new X-Men character"
+class Car
+attr_reader :model, :color
+attr_accessor :hp, :mph
+
+	def initialize(model, color, hp, mph)
+		@model = model
+		@color = color
+		@hp = hp.to_i
+		@mph = mph.to_i
+		"Initializing Awesome Carmaker 3000!!!!"
+	end
 	
+	def speed(x)
+		if x < 50
+		 "You car's max speed is #{x}! That's not very fast." 
+		elsif x < 150
+		"Your car's max speed is #{x}! That's fast!"
+		else
+		"Your car's max speed is #{x}! THAT'S RECORD BREAKING!!!!"
+		end
+	end
+	
+	def drives
+		"VROOOOOOOOOOOOOOOM!!!!!!!! ERRRRRRRRRRRRRR!!!!!!"
 	end
 
-	@mutant_level = [1,2,3,4,5]
-	@mutant = {"Jean Gray" => "Telepathy", "Mystique" => "shapeshifting", "Cyclops" => "laser eyes", "Wolverene" => "health regeneration", "Storm" => "weather control", "Magnito" => "metal control" }
-	@gender = ["Male", "Female"]
-
-
-	def attacks
-		mutant_array = @mutant.to_a
-		mutant_array[rand(0..5)]
-	end
-
-	def method_name
-		
-	end
-
-	def method_name
-		
-	end
 end
 
-player1 = X_Men_Characters.new
-p player1.attacks
-
-#Release 1  ----------------------------------------
-=begin
-use attr_reader and attr_accessor
-use def initalize
-	end 
-=end	
-
-
-#add interface
-p "This game can have up to 6 players. How many players are there?"
-num_of_players = gets.chomp.to_i
-
-num_of_players.times do 
-	new_player = X_Men_Characters.new	
-end
-
-
+	
