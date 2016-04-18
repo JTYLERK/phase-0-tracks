dbb251e75d3d73bb0cde83e91db7d1abf1d2f5b8
JtylerK <jefftyler89@gmail.com>
2016-04-17 22:02:14 -0700

    Added psuedocode

---
+++ js/algos.js
@@ -0,0 +1,5 @@
+// Release 0
+// create a function that takes an array as an arguement 
+// find the longest word in array and store as a variable
+// create a loop to go through every word in array and store the longest word in a variable
+// print that variable
