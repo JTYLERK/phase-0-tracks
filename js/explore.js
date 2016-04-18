f5889a59c0304ec820cdbff832aa21a4f65af6c0
JtylerK <jefftyler89@gmail.com>
2016-04-17 22:40:59 -0700

    Added reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,10 @@
 // create a function that reverses a string
 // use ruby methods to split the chars in a string to an array, call .reverse, and then join it back to the array.
+
+function reverse(str) {
+	return str.split("").reverse().join("");
+}
+
+
+//DRIVE CODE TEST FUNCTION
+console.log(reverse("try to reverse this string"));
