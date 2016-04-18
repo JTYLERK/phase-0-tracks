f1b2f17dc4a9f89cb6fa72510f2d337c1f04d2a9
JtylerK <jefftyler89@gmail.com>
2016-04-18 09:09:17 -0700

    Added release 1 pseudocode

--- js/algos.js
+++ js/algos.js
@@ -21,7 +21,19 @@ function Longestword(array) {
 }
 
 
-
 // DRIVER CODE
 var sample = ["long phrase","longest phrase","longer phrase", "this is the longest phrase"];
 console.log(Longestword(sample));
+
+
+
+// RELEASE 1
+// Create a matching function that takes two objects (2 parameters) and checks to see if the objects share
+// at least one key-value pair
+// set variable to 0 to check when match is found
+// need two loops. one sets the object to compare to find a match
+// if match found return true
+// else match not found return false
+
+
+
